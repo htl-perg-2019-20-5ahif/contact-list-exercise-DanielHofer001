a4b54870ee52460cef50e4b2358cee36daf30b22
Daniel Hofer <dh@x-net.at>
2019-09-30 09:07:16 +0200

    added project

---
+++ AdressBook/AdressBook/Controllers/AdressBookController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AdressBook.Controllers
+{
+    [ApiController]
+    [Route("/contacts")]
+    public class AdressBookController : ControllerBase
+    {   //CTRL ++RR
+        //CTRL + K+C/U
+        // CTRL + K F/CTRL+ K Fs
+        private static readonly List<Person> Contacts =
+                new List<Person> { };
+
+        [HttpGet]
+        public IActionResult GetAllContacts() => Ok(Contacts);
+
+        [HttpGet]
+        [Route("{index}", Name = "GetSpecificContact")]
+        public IActionResult GetContact(int index)
+        {
+
+            if (Contacts.Find(i => i.id == index) == null)
+            {
+                return NotFound("Person not found");
+            }
+            return Ok(Contacts.Where(i => i.id == index).FirstOrDefault());
+        }
+        //https://stackoverflow.com/questions/1485766/finding-an-item-in-a-list-using-c-sharp
+        [HttpPost]
+        public IActionResult AddContact([FromBody] Person newContact)
+        {
+            
+            if (Contacts.Find(i => i.id == newContact.id) == null)
+            {
+                Contacts.Add(newContact);
+                return CreatedAtRoute("GetSpecificContact", new { index = Contacts.IndexOf(newContact) }, newContact);
+
+            }
+
+            return BadRequest("Person already exists");
+        }
+        [HttpPut]
+        [Route("{index}")]
+        public IActionResult UpdateContact(int index, [FromBody] Person newContact)
+        {
+            if (index >= 0 && index < Contacts.Count)
+            {
+                Contacts[index] = newContact;
+                return Ok();
+            }
+
+            return BadRequest("Invalid index");
+        }
+        [HttpDelete]
+        [Route("{index}")]
+        public IActionResult DeleteContact(int index)
+        {
+            try
+            {
+                if (Contacts.Single(x => x.id == index) != null)
+                {
+                    Contacts.Remove(Contacts.Single(x => x.id == index));
+                    return Ok($"Deleted contact with id {index}");
+                }
+            }
+            catch
+            {
+                return NotFound("Person not found");
+            }
+            return BadRequest("Invalid id");
+        }
+        [HttpGet]
+        [Route("findByName", Name = "GetContactByName")]
+        public IActionResult FindContactByName([FromQuery]string nameFilter)
+        {
+            if (nameFilter != null)
+            {
+                IEnumerable<Person> c = (Contacts.Where(i => i.firstName.Contains(nameFilter) || i.lastName.Contains(nameFilter)));
+                if (c.Count() > 0)
+                {
+                    return Ok(c);
+                }
+            }
+          
+            return BadRequest("Invalid or missing name");
+        }
+    }
+}
+
---
+++ AdressBook/AdressBook/Person.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace AdressBook
+{
+    public class Person
+    {
+
+        public Int32 id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+
+    }
+}
